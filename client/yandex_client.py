96dd42603cf335f188be4e150b5d58f1e2928946
AliRN <ali_ramazanov_2000@mail.ru>
2025-03-13 23:37:38 +0300

    fix bug:yandex auth

--- client/yandex_client.py
+++ client/yandex_client.py
@@ -14,8 +14,8 @@ class YandexClient:
 
     async def get_user_info(self, code: str) -> YandexUserData:
         access_token = await self._get_user_access_token(code=code)
-        async with self.async_client as client:
-            user_info = await client.get('https://login.yandex.ru/info?format=json',
+        # async with self.async_client as client:
+        user_info = await self.async_client.get('https://login.yandex.ru/info?format=json',
                                  headers={'Authorization': f'OAuth {access_token}'})
         return YandexUserData(**user_info.json(), access_token=access_token)
 
@@ -27,7 +27,6 @@ class YandexClient:
             'client_secret': self.settings.YANDEX_CLIENT_SECRET,
         }
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        async with self.async_client as client:
-            response = await client.post(self.settings.YANDEX_TOKEN_URL, data=data, headers=headers)
+        response = await self.async_client.post(self.settings.YANDEX_TOKEN_URL, data=data, headers=headers)
         access_token = response.json()['access_token']
         return access_token
